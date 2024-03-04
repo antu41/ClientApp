e37180e1f4db0cc42bcaf0d5508242cb0a0b84cf
Antu <antu444475@email.com>
2024-03-04 23:51:17 +0600

    Add project files.

---
+++ ClientApp/Controllers/EmployeeController.cs
@@ -0,0 +1,136 @@
+﻿using ClientApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Policy;
+using System.Text;
+
+namespace ClientApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private string url = "https://localhost:7040/api/EmployeeAPI/";
+        private HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<List<Employee>>(result);
+                if (data != null)
+                {
+                    employees = data;
+                }
+            }
+            return View(employees);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            string data = JsonConvert.SerializeObject(emp);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["insert_message"] = "Employee Added..";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Employee emp = new Employee();
+            HttpResponseMessage response = client.GetAsync(url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Employee>(result);
+                if (data != null)
+                {
+                    emp = data;
+                }
+
+            }
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            string data = JsonConvert.SerializeObject(emp);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync(url + emp.id, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["update_message"] = "Employee Updated..";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Employee emp = new Employee();
+            HttpResponseMessage response = client.GetAsync(url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Employee>(result);
+                if (data != null)
+                {
+                    emp = data;
+                }
+
+            }
+
+            return View(emp);
+        }
+
+        [HttpGet]
+        public IActionResult Delete (int id)
+        {
+            Employee emp = new Employee();
+            HttpResponseMessage response = client.GetAsync(url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Employee>(result);
+                if (data != null)
+                {
+                    emp = data;
+                }
+
+            }
+
+            return View(emp);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfiremd(int id)
+        {
+            HttpResponseMessage response = client.DeleteAsync(url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["delete_message"] = "Employee Deleted..";
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+        }
+    }
+
+
+}
---
+++ ClientApp/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientApp.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string department { get; set; }
+        [Required]
+        public string designation { get; set; }
+    }
+}
